3b498e60919606d4b6e08983d7864560a278cb60
Abhiraj Yadav <abhiraj100right@gmail.com>
2024-10-04 20:56:58 +0530

    Pass data/state from One Page to Another

--- src/Components/CountriesList.jsx
+++ src/Components/CountriesList.jsx
@@ -1,50 +1,101 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react/prop-types */
-import React, { useEffect, useState } from "react";
-import CountryCard from "./CountryCard";
-import CountriesListShimmer from "./CountriesListShimmer";
+// /* eslint-disable no-unused-vars */
+// /* eslint-disable react/prop-types */
+import React, { useEffect, useState } from 'react'
+// import countriesData from '../countriesData'
+import CountryCard from './CountryCard'
+import CountriesListShimmer from './CountriesListShimmer'
 
-const CountriesList = ({ query }) => {
-  let [countriesData, setCountriesData] = useState([]);
+export default function CountriesList({ query }) {
+  const [countriesData, setCountriesData] = useState([])
+
   useEffect(() => {
-    fetch("https://restcountries.com/v3.1/all")
+    fetch('https://restcountries.com/v3.1/all')
       .then((res) => res.json())
       .then((data) => {
-        setCountriesData(data);
-      });
-  }, []);
+        setCountriesData(data)
+      })
+  }, [])
+
+  if (!countriesData.length) {
+    return <CountriesListShimmer />
+  }
 
   // if(countriesData.length === 0){
   //   return <CountriesListShimmer />
   // }
-
+  
   return (
     <>
-      {!countriesData.length ? (
-        <CountriesListShimmer />
-      ) : (
-        <div className="countries-container">
-          {countriesData
-            .filter((country) =>
-              country.name.common.toLowerCase().includes(query)
+      <div className="countries-container">
+        {countriesData
+          .filter((country) =>
+            country.name.common.toLowerCase().includes(query)
+          )
+          .map((country) => {
+            return (
+              <CountryCard
+                key={country.name.common}
+                name={country.name.common}
+                flag={country.flags.svg}
+                population={country.population}
+                region={country.region}
+                capital={country.capital?.[0]}
+                data={country}
+              />
             )
-            .map((country) => {
-              return (
-                <CountryCard
-                  key={country.name.common}
-                  name={country.name.common}
-                  flag={country.flags.svg}
-                  population={country.population}
-                  region={country.region}
-                  capital={country.capital?.[0]}
-                />
-              );
-            })}
-          ;
-        </div>
-      )}
+          })}
+      </div>
     </>
-  );
-};
+  )
+}
 
-export default CountriesList;
+
+
+// import React, { useEffect, useState } from "react";
+// import CountryCard from "./CountryCard";
+// import CountriesListShimmer from "./CountriesListShimmer";
+
+// const CountriesList = ({ query }) => {
+//   let [countriesData, setCountriesData] = useState([]);
+//   useEffect(() => {
+//     fetch("https://restcountries.com/v3.1/all")
+//       .then((res) => res.json())
+//       .then((data) => {
+//         setCountriesData(data);
+//       });
+//   }, []);
+
+//   // if(countriesData.length === 0){
+//   //   return <CountriesListShimmer />
+//   // }
+
+//   return (
+//     <>
+//       {!countriesData.length ? (
+//         <CountriesListShimmer />
+//       ) : (
+//         <div className="countries-container">
+//           {countriesData
+//             .filter((country) =>
+//               country.name.common.toLowerCase().includes(query)
+//             )
+//             .map((country) => {
+//               return (
+//                 <CountryCard
+//                   key={country.name.common}
+//                   name={country.name.common}
+//                   flag={country.flags.svg}
+//                   population={country.population}
+//                   region={country.region}
+//                   capital={country.capital?.[0]}
+//                 />
+//               );
+//             })}
+//           ;
+//         </div>
+//       )}
+//     </>
+//   );
+// };
+
+// export default CountriesList;
--- src/Components/CountryCard.jsx
+++ src/Components/CountryCard.jsx
@@ -1,26 +1,23 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 
-const CountryCard = ({ name, flag, population, region, capital }) => {
+export default function CountryCard({ name, flag, population, region, capital, data }) {
   return (
-    // <Link className="country-card" to={`/country?name=${name}`}>
-    <Link className="country-card" to={`/${name}`}>
-    <img src={flag} alt={name + ' Flag'} />
-    <div className="card-text">
-      <h3 className="card-title">{name}</h3>
-      <p>
-        <b>Population: </b>
-        {population.toLocaleString('en-IN')}
-      </p>
-      <p>
-        <b>Region: </b>{region}
-      </p>
-      <p>
-        <b>Capital: </b>{capital}
-      </p>
-    </div>
-  </Link>
+    <Link className="country-card" to={`/${name}`} state={data}>
+      <img src={flag} alt={name + ' Flag'} />
+      <div className="card-text">
+        <h3 className="card-title">{name}</h3>
+        <p>
+          <b>Population: </b>
+          {population.toLocaleString('en-IN')}
+        </p>
+        <p>
+          <b>Region: </b>{region}
+        </p>
+        <p>
+          <b>Capital: </b>{capital}
+        </p>
+      </div>
+    </Link>
   )
 }
-
-export default CountryCard;
